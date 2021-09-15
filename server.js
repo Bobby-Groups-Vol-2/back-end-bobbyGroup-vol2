5608a7bcc61b45328b81f16496ced1f3b98816fa
Ubuntu <azureuser@offmeowbackend.lytwmobnejwuvne3wg5krk4i1d.ix.internal.cloudapp.net>
2021-09-15 10:06:09 +0000

    [UPDATE]mysql

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.listen(PORT, () => {
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to OffMeow application. Hello world Dev" });
+  res.json({ message: "Welcome to OffMeow application. Hello world Dev UPDATE DB" });
 });
 
 app.get("/test", (req, res) => {
