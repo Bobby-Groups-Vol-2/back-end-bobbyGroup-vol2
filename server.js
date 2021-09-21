8d80d7f641485de6377e858a3c09487f920d749b
SURAPONG CHAMALAI <surapong.bird@mail.kmutt.ac.th>
2021-09-21 12:42:23 +0700

    [cors]round 2

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const cors   = require("cors");
 const app = express();
 const db = require("./app/models");
 const PORT = process.env.PORT || 5000;
+const multer = require('multer');
 
-
+app.use(multer({dest:'./uploads/'}).single('file'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE')
   res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
   next()
@@ -32,14 +33,16 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/upload.html');
 });
 
+app.post('/upload', function (req, res) {
+  res.send(req.files)
+})
+
 app.get("/test", (req, res) => {
   res.json({ message: "It's work"})
 })
 
 
-app.post('/upload', function (req, res) {
-  res.send(req.files)
-})
+
 
 require("./app/routes/vaccines.routes")(app);
 require("./app/routes/species.routes")(app);
