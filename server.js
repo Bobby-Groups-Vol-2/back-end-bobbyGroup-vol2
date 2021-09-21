a3b53b7e5bf707915d23fc00d7319b409ab96108
SURAPONG CHAMALAI <surapong.bird@mail.kmutt.ac.th>
2021-09-21 12:32:55 +0700

    [cors]

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
+  next()
+})
 db.sequelize.sync({ alter : true }).then(() => {
   console.log("offmeow works");
 });
@@ -23,11 +28,17 @@ app.listen(PORT, () => {
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to OffMeow application. oi looking good ได้ทีเถอะ" });
+ 
+  res.sendFile(__dirname + '/upload.html');
 });
 
 app.get("/test", (req, res) => {
   res.json({ message: "It's work"})
+})
+
+
+app.post('/upload', function (req, res) {
+  res.send(req.files)
 })
 
 require("./app/routes/vaccines.routes")(app);
